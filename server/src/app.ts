07f98a397c168a6785801a955390e06c2584db0d
Juan Velez <juanvelez.dev@gmail.com>
2023-11-03 15:59:10 -0500

    Juan<feature>: route products

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
 import userRouter from './routes/users.routes'
+import productsRouter from './routes/products.routes'
 import cors from 'cors'
 
 const app = express()
-// middleware for app
+// middleware
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/user', userRouter)
+app.use('/products', productsRouter)
 app.use(cors())
 
 export default app
--- server/src/controllers/products/getAllProducts.ts
+++ server/src/controllers/products/getAllProducts.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const getAllProducts = async () => {
   try {
     const { data } = await axios('https://fakestoreapi.com/products')
