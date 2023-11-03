725d4552b0d378938a6e616635fe8baa37cc5403
Juan Velez <juanvelez.dev@gmail.com>
2023-11-03 15:35:15 -0500

    Juan<feature>: route user

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
-import router from './routes/users.routes'
+import userRouter from './routes/users.routes'
 import cors from 'cors'
 
 const app = express()
 // middleware for app
 app.use(morgan('dev'))
 app.use(express.json())
-app.use('/api', router)
+app.use('/user', userRouter)
 app.use(cors())
 
 export default app
