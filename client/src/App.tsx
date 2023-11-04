403355556628b66605759599c8d5cc6553b19b39
Jose Lozano <joselozano.dev@gmail.com>
2023-11-04 12:50:29 -0400

    Add NavBar component to App.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './components/Home/HomePage'
+import NavBar from './components/NavBar/NavBar'
 import './App.css'
 
 function App() {
   return (
     <>
+      <NavBar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
       </Routes>
