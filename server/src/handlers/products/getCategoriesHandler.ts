82ec0785f023cf30ca075c90430a087daadd5e3d
Juan Velez <juanvelez.dev@gmail.com>
2023-11-03 21:08:34 -0500

    Juan<feature>: /products/categories route completed

---
+++ server/src/handlers/products/getCategoriesHandler.ts
@@ -0,0 +1,14 @@
+import { type RequestHandler } from 'express'
+import getAllCategories from '../../controllers/products/getAllCategories'
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+const getCategoriesHandler: RequestHandler = async (_req, res) => {
+  try {
+    const categories: string[] | null = await getAllCategories()
+    if (categories !== null) res.status(200).json(categories)
+  } catch (error) {
+    res.status(500).json((error as Error).message)
+  }
+}
+
+export default getCategoriesHandler
--- server/src/routes/products.routes.ts
+++ server/src/routes/products.routes.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import getAllProductsHandler from '../handlers/products/getAllProductsHandler'
+import getCategoriesHandler from '../handlers/products/getCategoriesHandler'
 
 const productsRouter = express.Router()
 productsRouter.get('/', getAllProductsHandler)
+productsRouter.get('/categories', getCategoriesHandler)
 
 export default productsRouter
