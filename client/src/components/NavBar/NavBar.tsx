8f0887ed4185aac5ffe9f44ffbd132c8f3baf35a
Jose Lozano <joselozano.dev@gmail.com>
2023-11-05 22:45:16 -0400

    Refactor CSS styles and update Card component props

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,39 +1,59 @@
-import './NavBar.css'
-import { Link } from 'react-router-dom'
-import { motion } from 'framer-motion'
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+import { motion, Variants } from "framer-motion";
+import { useState } from "react";
+
+const menuVariants: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState("hidden");
+
   return (
-    <motion.div 
-    className='navBar-Container'
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{duration: 1 }}
+    <motion.div
+      className="navBar-Container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
     >
-        <h1 className='navBar-title'>FakeStore</h1>
-        <div>
-            <ul className='navBar-links-container'>
-              <motion.li
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              >
-                <Link to='/'>Home</Link>
-              </motion.li>
-              <motion.li
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              >
-                <Link to='/about'>About</Link>
-              </motion.li>
-              <motion.li
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              >
-                <Link to='/shopping-cart'>My Cart</Link>
-              </motion.li>
-            </ul>
-        </div>
-        <div>
+      <h1 className="navBar-title">FakeStore</h1>
+      <div>
+        <motion.button
+          className="navBar-menu-btn"
+          onClick={() => {
+            menuOpen === "hidden"
+              ? setMenuOpen("visible")
+              : setMenuOpen("hidden");
+          }}
+        >
+          Menu
+        </motion.button>
+        <motion.div
+          className="navBar-menu-layout"
+          variants={menuVariants}
+          animate={menuOpen}
+          onClick={() => { setMenuOpen("hidden")}}
+        >
+          <motion.ul className="navBar-links-container">
+            <motion.li whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+              <Link to="/">Home</Link>
+            </motion.li>
+            <motion.li whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+              <Link to="/about">About</Link>
+            </motion.li>
+            <motion.li whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+              <Link to="/shopping-cart">My Cart</Link>
+            </motion.li>
+          </motion.ul>
+        </motion.div>
+      </div>
+      {/* <div>
             <input type="text" className='search-input'/>
             <button className='search-btn'>
               <img 
@@ -42,9 +62,9 @@ function NavBar() {
               className='search-icon'
               />
             </button>
-        </div>
+        </div> */}
     </motion.div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
